ea15c40a6c596e0820db1b0cf4ba6c6d2c1eb9df
brad-basilio <brad.basilioppy@gmail.com>
2025-05-27 11:00:08 -0500

    seeders

--- database/seeders/WebsiteStatisticSeeder.php
+++ database/seeders/WebsiteStatisticSeeder.php
@@ -13,14 +13,27 @@ class WebsiteStatisticSeeder extends Seeder
      */
     public function run(): void
     {
-        // Poblar con 30 días de visitas aleatorias
-        for ($i = 0; $i < 30; $i++) {
+        // Poblar con 100 registros de visitas aleatorias
+        $referrers = ['google.com', 'facebook.com', 'twitter.com', 'direct', 'linkedin.com'];
+        $sources = ['ad', 'organic', 'referral', 'email'];
+        $devices = ['iPhone', 'Android', 'Windows PC', 'MacBook', 'iPad'];
+        $deviceTypes = ['mobile', 'desktop', 'tablet'];
+        $oss = ['iOS', 'Android', 'Windows', 'macOS', 'Linux'];
+        $browsers = ['Chrome', 'Firefox', 'Safari', 'Edge', 'Opera'];
+        $locations = ['USA', 'UK', 'Canada', 'Australia', 'India'];
+        $pages = ['/home', '/about', '/contact', '/pricing', '/blog'];
+
+        for ($i = 0; $i < 100; $i++) {
             WebsiteStatistic::create([
-                'visits' => rand(50, 500),
-                'date' => Carbon::now()->subDays($i)->toDateString(),
-                // Agrega aquí otros campos si existen, por ejemplo:
-                // 'user_id' => 1,
-                // 'page' => 'home',
+            'referrer'    => $referrers[array_rand($referrers)],
+            'source'      => $sources[array_rand($sources)],
+            'device'      => $devices[array_rand($devices)],
+            'device_type' => $deviceTypes[array_rand($deviceTypes)],
+            'os'          => $oss[array_rand($oss)],
+            'browser'     => $browsers[array_rand($browsers)],
+            'location'    => $locations[array_rand($locations)],
+            'page'        => $pages[array_rand($pages)],
+            'visited_at'  => Carbon::now()->subMinutes(rand(0, 60 * 24 * 30)),
             ]);
         }
     }
